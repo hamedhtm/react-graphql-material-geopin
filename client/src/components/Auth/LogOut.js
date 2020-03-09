1c17ed85696a316b296d669a920b5e2abbd86a2b
PHOENIX <hamed4000@yahoo.com>
2020-03-09 19:35:44 +0330

    header and logOut

---
+++ client/src/components/Auth/LogOut.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { GoogleLogout } from 'react-google-login';
+import ExitToApp from '@material-ui/icons/ExitToApp';
+import Typography from '@material-ui/core/Typography';
+import { AuthContext } from '../../AuthContext';
+
+const LogOut = ({ classes }) => {
+  const { logOut } = useContext(AuthContext);
+
+  return (
+    <GoogleLogout
+      onLogoutSuccess={() => logOut()}
+      render={({ onClick }) => (
+        <span className={classes.root} onClick={onClick}>
+          <Typography variant={'body1'} className={classes.buttonText}>
+            LogOut
+          </Typography>
+          <ExitToApp className={classes.buttonIcon} />
+        </span>
+      )}
+    />
+  );
+};
+
+const styles = {
+  root: {
+    cursor: 'pointer',
+    display: 'flex',
+  },
+  buttonText: {
+    color: 'orange',
+  },
+  buttonIcon: {
+    marginLeft: '5px',
+    color: 'orange',
+  },
+};
+
+export default withStyles(styles)(LogOut);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,36 +1,72 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Map from "@material-ui/icons/Map";
-// import Typography from "@material-ui/core/Typography";
+import React, { useContext } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MapIcon from '@material-ui/icons/Map';
+import Typography from '@material-ui/core/Typography';
+import { AuthContext } from '../AuthContext';
+import LogOut from './Auth/LogOut';
 
 const Header = ({ classes }) => {
-  return <div>Header</div>;
+  const {
+    state: { currentUser },
+  } = useContext(AuthContext);
+  return (
+    <div className={classes.root}>
+      <AppBar position={'static'}>
+        <Toolbar>
+          <div className={classes.grow}>
+            <MapIcon className={classes.icon} />
+            <Typography
+              component={'h1'}
+              variant={'h6'}
+              color={'inherit'}
+              noWrap
+            >
+              GeoPins
+            </Typography>
+          </div>
+          {currentUser && (
+            <div className={classes.grow}>
+              <img
+                src={currentUser.picture}
+                alt={currentUser.name}
+                className={classes.picture}
+              />
+              <Typography variant={'h5'} color={'inherit'} noWrap>
+                {currentUser.name}
+              </Typography>
+            </div>
+          )}
+          <LogOut />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 const styles = theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   grow: {
     flexGrow: 1,
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   icon: {
     marginRight: theme.spacing.unit,
-    color: "green",
-    fontSize: 45
+    color: 'green',
+    fontSize: 45,
   },
   mobile: {
-    display: "none"
+    display: 'none',
   },
   picture: {
-    height: "50px",
-    borderRadius: "90%",
-    marginRight: theme.spacing.unit * 2
-  }
+    height: '50px',
+    borderRadius: '90%',
+    marginRight: theme.spacing.unit * 2,
+  },
 });
 
 export default withStyles(styles)(Header);
